5fe88b655842197133581cc9c427ae59f80af895
Kevin <kevinkenny92@gmail.com>
2017-07-04 01:47:57 +0000

    files

--- dummy.js
+++ dummy.js
@@ -1,17 +1,15 @@
 var request = require('request');
 var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!\n')
-//The \n character is used to find a newline character.
 
-var GITHUB_USER = "kevinkenny92";
-var GITHUB_TOKEN = "0d7cdc2953cd259e6b2ea29a3265f4cee7a7872f";
+console.log('Welcome to the GitHub Avatar Downloader!\n');
+
+var GITHUB_USER = 'arnoldthchan';
+var GITHUB_TOKEN = 'b56d31ba5c5cb58f1f0a55a2f9d9cf22877f0603';
 
 function complete(){
-  console.log('COMPLETED!');
+  console.log('Download completed')
 }
-
-
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function (err) {
@@ -23,25 +21,29 @@ function downloadImageByURL(url, filePath) {
          .pipe(fs.createWriteStream(filePath + '.jpg').on('finish', complete));
 }
 
+//Fetches contributors as a JSON list
+//Involves a callback function
+//to loop and print out avatar URLs, for each object
 function getRepoContributors(repoOwner, repoName, callback) {
-  var requestURL = {
+var options = {
         url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-        headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'
-          }
-      };
+        headers: {
+            'User-Agent': 'GitHub Avatar Downloader - Student Project'
+        }
+    };
 
-
-request(requestURL, function(err, res, body){
+request(options, function(err, res, body){
   if (err) throw err;
+
   var text = JSON.parse(body);
   for (i in text){
-    console.log(text[i].avatar_url);
-    console.log(`avatars/${text[i].login}`)
- }
+
+  }
   });
 }
 
-
+//https://github.com/nodejs/node
+//https://github.com/jquery/jquery
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log('Errors:', err);
   console.log('Result:', result);
